20619a8c77707a90ae6d679d1c54ecb181206a5e
Rehab Ragab <67760628+RehabAbotalep@users.noreply.github.com>
2024-09-18 22:01:40 +0100

    Update variables.tf

--- src/iac/variables.tf
+++ src/iac/variables.tf
@@ -1,9 +1,4 @@
 # iac/variables.tf
-variable "env_name" {
-  description = "The name of the environment"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
