e51719cb6279e6e867c5638f2c52ec720403b2b4
Rehab Ragab <67760628+RehabAbotalep@users.noreply.github.com>
2025-05-09 12:53:14 +0100

    Update install-lamp.sh

--- src/iac/scripts/install-lamp.sh
+++ src/iac/scripts/install-lamp.sh
@@ -3,6 +3,24 @@
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+
+sudo apt install xfce4-session -y
+
+sudo apt-get -y install xrdp
+
+sudo adduser xrdp ssl-cert
+
+echo xfce4-session >~/.xsession
+
+sudo service xrdp restart
+
+sudo apt install firefox -y
+
+sudo apt install unzip
+
+sudo apt install unar
+
 # Install LAMP
 sudo apt install lamp-server^ -y
 
